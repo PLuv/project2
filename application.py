865b06ad2c1455d6b4bc03c3bac3f8647a791cc3
Philip Walter Love <pwl679@g.harvard.edu>
2018-07-15 21:15:18 +0000

    Creating index.html

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
 import os
 
-from flask import Flask
+#flask import session is client side session.
+from flask import Flask, session, render_template, request, redirect, jsonify
+
+#flask_session is server side.
+from flask_session import Session
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -12,4 +16,4 @@ channel_list = ['general']
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html")
---
+++ static/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    function hello() {
+        document.querySelector('h3').innerHTML = 'GoodBye!';
+    }
+// End of file.
+});
