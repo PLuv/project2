f1ee029d6f6e46d7eb1fe49ab889d4e41617fdb2
Philip Walter Love <pwl679@g.harvard.edu>
2018-07-16 15:58:25 +0000

    Begin work on user session

--- application.py
+++ application.py
@@ -7,13 +7,20 @@ from flask import Flask, session, render_template, request, redirect, jsonify
 from flask_session import Session
 from flask_socketio import SocketIO, emit
 
+# Configure application
 app = Flask(__name__)
+
+# Check for environment variables
+if not os.getenv("SECRET_KEY"):
+    raise RuntimeError("SECRET_KEY is not set in os")
+
+# Set up SECRET_KEY
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 # list of all channels
 channel_list = ['general']
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
