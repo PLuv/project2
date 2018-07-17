9d53c6ee9c9d5aef62cc434889d9e5ecd7459198
Philip Walter Love <pwl679@g.harvard.edu>
2018-07-17 03:31:21 +0000

    At least ajax is working

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
-import os
+import requests, os
 
 #flask import session is client side session.
-from flask import Flask, session, render_template, request, redirect, jsonify
+from flask import Flask, session, render_template, request, jsonify
 
 #flask_session is server side.
 from flask_session import Session
@@ -14,6 +14,11 @@ app = Flask(__name__)
 if not os.getenv("SECRET_KEY"):
     raise RuntimeError("SECRET_KEY is not set in os")
 
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # Set up SECRET_KEY
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
@@ -21,6 +26,15 @@ socketio = SocketIO(app)
 # list of all channels
 channel_list = ['general']
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("test2.html")
+
+@app.route("/check_in", methods=["POST"])
+def check_in():
+
+    new_user = request.form.get("new_user")
+    data = new_user
+    print(data)
+
+    return jsonify({"success": True, "rate": data})
--- static/scripts.js
+++ static/scripts.js
@@ -1,7 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Check if username exists on localStorage.
+    if (localStorage.getItem("username") === null) {
+        document.querySelector('#modal_1').style.display = "block";
 
-    function hello() {
-        document.querySelector('h3').innerHTML = 'GoodBye!';
+        // Read form username.
+        document.querySelector('#form1').onsubmit = () => {
+
+            // initialize new request
+            const request = new XMLHttpRequest();
+            const new_user = document.querySelector('#user_name').value;
+            console.log(new_user);
+
+            // open request
+            request.open('POST', '/check_in');
+
+            // Callback function for when request completes
+            request.onload = () => {
+
+                // Extract JSON data from request
+                const data = JSON.parse(request.responseText);
+
+                // update the result div
+                if (data.success) {
+                    const contents = `You are now logged in as ${data.rate} ${new_user}!`
+                    document.querySelector('#result').innerHTML = contents;
+                    console.log("Success recieved");
+                }
+                else {
+                    document.querySelector('#result').innerHTML = 'There was an error logging you in.';
+                }
+            }
+
+            // Add data to send with request
+            const data = new FormData();
+            data.append('new_user', new_user);
+
+            // Send request
+            request.send(data);
+            return false;
+        };
+
     }
-// End of file.
+
+//End of file
 });
